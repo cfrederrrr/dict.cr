2ec3d8c037b57bf541ffecbad2be1848ad342449
galvertez <galvertez@gmail.com>
2019-06-11 21:09:19 -0400

    add tests for english versions of insertion and lookup

--- spec/dict_spec.cr
+++ spec/dict_spec.cr
@@ -17,6 +17,12 @@ describe Dict do
     end
   end
 
+  describe "#lookup" do
+    it "returns the associated value" do
+      Dict::SPEC_INSTANCE["health"].should eq(Digest::SHA1.hexdigest("health"))
+    end
+  end
+
   describe "#[]=" do
     dict = Dict(String, String).new
 
@@ -32,6 +38,22 @@ describe Dict do
       dict["something"] = "abcdefghijklmnopqrstuvwxyz"
       dict["something"].should eq("abcdefghijklmnopqrstuvwxyz")
     end
+  end
 
+  describe "#define" do
+    dict = Dict(String, String).new
+
+    it "accepts words and definitions" do
+      dict["something"] = "something else"
+      dict["nothing"] = "nothing else"
+
+      dict["something"].should eq("something else")
+      dict["nothing"].should eq("nothing else")
+    end
+
+    if "overwrites existing values" do
+      dict["something"] = "abcdefghijklmnopqrstuvwxyz"
+      dict["something"].should eq("abcdefghijklmnopqrstuvwxyz")
+    end
   end
 end
